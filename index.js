113edb0e5c618273fdc447c004a451e362a33cce
Delanie Perez <delanieperez11@gmail.com>
2018-05-09 22:21:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  recipes['prop'] = 1;
-  recipes['prop2'] = 2
-  return recipes
+  return Object.assign({}, object, {[key]: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
