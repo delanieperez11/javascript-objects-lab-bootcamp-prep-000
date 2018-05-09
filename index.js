6a38769559f650c1231e12f59b90eae89cd4b6ac
Delanie Perez <delanieperez11@gmail.com>
2018-05-09 21:57:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var recipes = { recipe: 'object'}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  recipes['prop'] = 1;
+  recipes['prop2'] = 2
+  return recipes
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  Object.assign({}, recipes, { ['prop']: 1 })
+  Object.assign({}, recipes, { ['prop2']: 2 })
+  return recipes
+}
+
